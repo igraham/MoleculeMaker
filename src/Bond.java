095c48df4046808a0c698b8dfbcf4f2a31c5b814
Brennan Maddox <brennan@b.local>
2012-02-20 21:22:02 -0500

    Added two comments to Bond

--- src/Bond.java
+++ src/Bond.java
@@ -7,8 +7,8 @@ import javax.swing.JButton;
 
 public class Bond //extends JButton //implements ActionListener
 {	
-	private Element bonder;
-	private Element bondee;
+	private Element bonder; //bonder
+	private Element bondee; //bondee
 	private String bondKey;
 	
 	public Bond(Element bonder, Element bondee)
